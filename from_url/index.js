bbe8583d287b6ce158c3ac10f0d8cd0f143f9e2f
chickencuber <n1batman728@gmail.com>
2026-02-11 19:28:13 -0600

    fixed another bug

--- from_url/index.js
+++ from_url/index.js
@@ -1,7 +1,7 @@
 return new Promise((r) => {
     const p = prompt("url").trim();
     if(p === "") return ["", ""];
-    fetch().then(v => v.text().then(v => {
+    fetch(p).then(v => v.text().then(v => {
         r([v, crypto.randomUUID()]);
     }))
 })
