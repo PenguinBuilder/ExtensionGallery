b23af9bfc887946e2c7e6924406116b8386f470e
chickencuber <n1batman728@gmail.com>
2026-02-11 19:59:03 -0600

    cleaned something

--- from_url/index.js
+++ from_url/index.js
@@ -1,6 +1,6 @@
 return new Promise((r) => {
     const p = prompt("url").trim();
-    if(p === "") return ["", ""];
+    if(p === "") return;
     fetch(p).then(v => v.text().then(v => {
         r([v, crypto.randomUUID()]);
     }))
